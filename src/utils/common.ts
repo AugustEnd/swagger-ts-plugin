059c802d3d21ba8b0dccbb7ecbd5b7907eaee12e
gaofeng.ye <gaofeng.ye@mobilemd.cn>
2021-02-02 14:58:46 +0800

    '修改过滤特殊字符'

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,16 +1,14 @@
 const fs = require("fs");
 /**
- * @param key 含有处理特殊字符«»，如a«b«c»» 转换成a_b_c;
+ * @param key 含有处理特殊字符«» 【】 {} [] () （），如a«b«c»» 转换成a_b_c;
  */
 export const handleSpecialSymbol = (key: string | any) => {
     return typeof key !== "string"
         ? key
         : key
-              .replace(/«/g, "_")
-              .replace(/»/g, "")
-              .replace(/\(/g, "_")
-              .replace(/\)/g, "")
-              .replace(/[\?|\,|\.|\-|\/]/g, "");
+              .replace(/[\«|\(|\（|\【|\[|\{]/g, "_")
+              .replace(/[\»|\)|\）|\】|\]|\}]/g, "")
+              .replace(/[\?|\？|\,|\，|\.|\。|\-|\/|\、|\=]/g, "");
 };
 
 /**
