27bc17c4cd2ed061fb7bfda7e0fbf23b78c8c19d
gaofeng.ye <gaofeng.ye@mobilemd.cn>
2021-02-02 15:03:07 +0800

    '升级'

--- src/execute/index.ts
+++ src/execute/index.ts
@@ -4,13 +4,6 @@ const path = require("path");
 startCreate({
     ...defaultValue,
     outputPath: path.resolve(__dirname, "../../"),
-    serverList: [
-        {
-            serviceName: "xxx-service",
-            serviceUrl: "http://172.20.37.153:8200/api/",
-        },
-        "pv-io",
-        "omp-service",
-        "master-data-service",
-    ],
+    serverList: ["site-service", "omp-service"],
+    appUrl: "http://eureka.test.com:1111/eureka/apps",
 });
